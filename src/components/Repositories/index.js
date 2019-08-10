fc23552f1deef07d1f958f2f23585efb37d202f9
simoneas02 <simoneas02@hotmail.com>
2019-08-09 23:44:36 -0300

    update Repositories component and add some style

--- src/components/Repositories/index.js
+++ src/components/Repositories/index.js
@@ -1,43 +1,22 @@
-import React, { useState, useEffect } from "react";
-import { getRepositories } from "../../services/getRepositoriesData";
-import Repository from "../Repository";
+import React, { useState, useEffect } from 'react';
+import Repository from '../Repository';
 
-// const repos = [
-//   {
-//     id: 1,
-//     name: "workshops_wdp",
-//     description: "workshops_wdp Workshop content from Women Dev Projects.",
-//     language: "JavaScript",
-//     forks_count: 6,
-//     stargazers_count: 4,
-//     open_issues_count: 2,
-//     updated_at: "2018-11-19T00:12:18Z"
-//   },
-//   {
-//     id: 2,
-//     name: "womendevprojects",
-//     description: "Together! Developing Technology with Impact <3",
-//     language: "JavaScript",
-//     forks_count: 6,
-//     stargazers_count: 4,
-//     open_issues_count: 2,
-//     updated_at: "2018-11-19T00:12:18Z"
-//   }
-// ];
+import './repositories.css';
+import { getRepositories } from '../../services/getRepositoriesData';
 
-export default function Repositories() {
+const Repositories = () => {
   const [repositories, setRepositories] = useState([]);
 
   useEffect(() => {
-    async function fetchData() {
-      const result = await getRepositories("womenwhocoderecife");
+    const fetchData = async () => {
+      const result = await getRepositories('womenwhocoderecife');
       setRepositories(result.data);
-    }
+    };
     fetchData();
   }, []);
 
   return (
-    <div>
+    <ul className="repositories">
       {repositories.map(repository => (
         <Repository
           key={repository.id}
@@ -50,6 +29,8 @@ export default function Repositories() {
           updated_at={repository.updated_at}
         />
       ))}
-    </div>
+    </ul>
   );
-}
+};
+
+export default Repositories;
